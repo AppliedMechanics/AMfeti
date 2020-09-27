becbafe14af06a9f3cf71d995d0a74abfc498b0b
sivasanarul <sivasankar.arul@gmail.com>
2020-09-27 00:58:09 -0700

    more addition to scalability - logging

--- scalability/run_scalability.py
+++ scalability/run_scalability.py
@@ -7,8 +7,9 @@ import shutil
 import time, logging, json, subprocess
 import sys
 from datetime import datetime
+from amfeti.scalabilty.case_generator import *
 curdir = os.getcwd()
-
+header = '#' * 50
 
 if __name__ == '__main__':
     help_doc = ''' 
@@ -33,11 +34,12 @@ if __name__ == '__main__':
             
             method   : pseudoinverse of subdomain matrices. Default = splusps (others: cholsps, svd)
             algorithm: serial or parallel exec.             Default = ParallelFETIsolver (others: SerialFETIsolver)
+            solver   : dual interface algorithm             Default = GNRES (others: PCPG, future: MPPCPG, MPGNRES)
             tol      : tolerance of solver error norm.      Default = 1.0E-5
             precond  : Preconditioner type.                 Default - Identity (options: Lumped, Dirichlet, LumpedDirichlet, SuperLumped)
             
-            strong        : scalability test, Boolear True for strong and False for weak. Default = True
-            loglevel      : INFO, DEBUG, ERROR, WARNING, CRITICAL. Default = WARNING
+            strong        : scalability test, Boolean True for strong and False for weak. Default = True
+            loglevel      : logging.(INFO/ DEBUG/ ERROR/ WARNING/ CRITICAL). Default = logging.WARNING
             launcher_only : Boolean True to create scripts without launching solver. Default = False
             delete_files  : Boolean True to delete *.pkl files after mpirun. Default = True
             salomon       : {} dict with salomon parameters e.g. {'queue':'qexp','ncpus' : 24, 'default_time':30, 'effectivity': 0.7}.  Default = {}
@@ -55,10 +57,11 @@ if __name__ == '__main__':
                     'domainY'       : [1, 2, 3],
                     'method'        : 'splusps',
                     'FETI_algorithm': 'ParallelFETIsolver',
+                    'solver'        : 'GMRES',
                     'tol'           : 1.0E-5,
                     'precond'       : None,
                     'strong'        : True,
-                    'loglevel'      : 'WARNING',
+                    'loglevel'      : logging.INFO,
                     'launcher_only' : False,
                     'delete_files'  : True,
                     'salomon'       : {},
@@ -66,6 +69,42 @@ if __name__ == '__main__':
     # add default dict to local variables
     locals().update(default_dict)
 
+    ###############################   create folder to store information   ############################################
+    date_str = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    scalability_folder = os.path.join(curdir, date_str)
+    os.mkdir(scalability_folder)
+    # change to scalability local folder
+    os.chdir(scalability_folder)
+    ##############################################  mpi size ##########################################################
+    if len(domainY) != len(domainX):
+        logging.warning('DomainY list with different length of DomainX. Setting new DomainY')
+        domainY = [domainY[0]] * len(domainX)
+        logging.warning(('new DomainY = ', domainY))
+
+    max_mpi_size = (max(domainX) * max(domainY))
+    min_mpi_size = (min(domainX) * min(domainY))
+    ##############################################  logging  ##########################################################
+    logging.basicConfig(level=loglevel, filename='scalability_master.log', filemode='w', format='%(levelname)s - %(message)s')
+    logging.info(header)
+    logging.info('#############    SCALABILITY TEST  ###############')
+    logging.info(header)
+    logging.info(datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+    if strong:
+        logging.info('Performing STRONG parallel scalability.')
+    else:
+        logging.info('Performing WEAK parallel scalability.')
+    if precond is None:
+        logging.info('Preconditioner type  = %s' % 'Identity')
+    else:
+        logging.info('Preconditioner type  = %s' % precond)
+    logging.info('Solver method  = %s' %solver )
+    logging.info('Solver tolerance  = %2.2e' % tol)
+    logging.info('Pseudoinverse method  = %s' % method)
+    logging.info('Number of global divisions in X directions = %i' % divX)
+    logging.info('Number of global divisions in Y directions = %i' % divY)
+    logging.info('Maximum MPI size  = %i' % max_mpi_size)
+    logging.info('Minimum MPI size  = %i' % min_mpi_size)
+    ###################################################################################################################
     for domain_x, domain_y in zip(domainX, domainY):
         mpi_size = domain_x * domain_y
         # subdomain dimensions
@@ -76,5 +115,6 @@ if __name__ == '__main__':
         subdomain_divy = divY / domain_y
 
         K_dict, M_dict, B_dict, f_dict, K_effective_dict, f_effective_dict  = create_subdomain_matrices(n_domains_x=domain_x, n_domains_y=domain_y,
-                                                                                                        n_ele_x=subdomain_divx, n_ele_y=subdomain_divy,
+                                                                                                        n_ele_X_global=divX, n_ele_Y_global=divY,
                                                                                                         length=W, height=H, split_system=False)
+        logging.info(header)

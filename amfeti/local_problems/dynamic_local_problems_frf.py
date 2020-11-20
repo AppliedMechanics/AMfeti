fixed preconditioner issue-copy to avoid reference and halfscaling, square root scaling

amfeti/local_problems/dynamic_local_problems_frf.py
@@ -221,11 +221,11 @@ class LinearDynamicLocalProblemFRF(LocalProblemBase):
         external_solution_dict_scaled : dict
             preconditioned external solutions on the interfaces
         """
-        q_b_dict_scaled = self.scaling.apply(q_b_dict)
-
         if self.preconditioner is None:
+            q_b_dict_scaled = self._apply_halfscaling(copy(q_b_dict))
             external_solution_dict_scaled = q_b_dict_scaled
         else:
+            q_b_dict_scaled = self.scaling.apply(copy(q_b_dict))
             q_b_expanded = self._expand_external_solution(q_b_dict_scaled)
             external_solution = self.preconditioner.apply(q_b_expanded)
             external_solution_dict = self._distribute_to_interfaces(external_solution)
@@ -308,6 +308,10 @@ class LinearDynamicLocalProblemFRF(LocalProblemBase):
 
         return interface_dofs, interior_dofs
 
-
+    def _apply_halfscaling(self,q_b_dict):
+        external_solution_dict = {}
+        for interface_id, B in self.B.items():
+            external_solution_dict[interface_id] = np.divide(q_b_dict[interface_id],2)
+        return external_solution_dict
 
 

amfeti/scaling.py
@@ -10,7 +10,8 @@ Scaling module for the preconditioning-step of AMfeti
 import numpy as np
 from scipy.sparse import diags
 
-__all__ = ['MultiplicityScaling']
+__all__ = ['MultiplicityScaling',
+           'SquarerootMultiplicityScaling']
 
 
 class ScalingBase:
@@ -94,3 +95,36 @@ class MultiplicityScaling(ScalingBase):
 
         for key, B in B_dict.items():
             self.scaling_dict[key] = diags(1.0 / scaling_values[B.indices])
+
+class SquarerootMultiplicityScaling(ScalingBase):
+    """
+    Multiplicity-Scaling, that scales the interface-quantities based on the square root number of gaps belonging 
+    to each local degree of freedom    
+    """
+    def __init__(self):
+        super().__init__()
+
+    def update(self, B_dict):
+        """
+        Updating-method for Multiplicity-scaling, that extracts the multiplicity-factors for each interface-gap from the
+        local B-matrices
+
+        Parameters
+        ----------
+        B_dict : dict
+            B-matrices of each interface
+
+        Returns
+        -------
+        None
+        """
+        self.scaling_dict = dict()
+        scaling_values = None
+        for key, B in B_dict.items():
+            if scaling_values is None:
+                scaling_values = np.ones(B.shape[1])
+
+            scaling_values[B.indices] += 1
+
+        for key, B in B_dict.items():
+            self.scaling_dict[key] = diags(1.0 / np.sqrt(scaling_values[B.indices]))